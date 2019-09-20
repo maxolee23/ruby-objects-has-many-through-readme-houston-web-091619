87225bccd9194dd48fcd31848e9c9f5d7452c738
Max Lee <maxolee23@gmail.com>
2019-09-20 18:59:52 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,9 +14,9 @@ class Customer
     @@all
   end
 
-  def new_meal(customer, total, tip)
-    Meal.new(waiter, self, total, tip)
-  end
+  def new_meal(waiter, total, tip=0)
+      Meal.new(waiter, self, total, tip)
+    end
 
 
 
