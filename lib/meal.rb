1781050da049cc94dd66f461d01291e7d330599b
Max Lee <maxolee23@gmail.com>
2019-09-20 18:54:39 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,10 +1,17 @@
 class Meal
 
+  @@all = []
+
   def initialize(waiter, customer, total, tip)
     @waiter = waiter
     @customer = customer
     @total = total
     @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 
